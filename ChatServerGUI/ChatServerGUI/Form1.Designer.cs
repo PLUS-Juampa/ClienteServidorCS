93188397fde85ef9b3e4a75ffd087180b16e6424
Juampa <juampa249@gmail.com>
2017-11-27 08:01:37 -0600

    first commit

---
+++ ChatServerGUI/ChatServerGUI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ChatServerGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(405, 219);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(178, 263);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 300);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatServerGUI/ChatServerGUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChatServerGUI
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(string text);
+        TcpListener listener;
+        TcpClient client;
+        NetworkStream ns;
+        Thread t = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listener = new TcpListener(4545);
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            ns = client.GetStream();
+            t = new Thread(DoWork);
+            t.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = "\n" + textBox2.Text + "\n";
+            byte[] byteTime = Encoding.ASCII.GetBytes(s);
+            ns.Write(byteTime, 0, byteTime.Length);
+        }
+
+        // This is run as a thread
+        public void DoWork()
+        {
+            byte[] bytes = new byte[1024];
+            while (true)
+            {
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+                this.SetText(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+                //MessageBox.Show(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+            }
+        }
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox2.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox2.Text = this.textBox2.Text + text;
+            }
+        }
+    }
+}
